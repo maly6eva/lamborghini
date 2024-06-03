d06ccffc74c556801888f406e3eb41284fadd99f
Ksenia Malysheva <maly6eva.ksenia@gmail.com>
2024-06-03 22:46:58 +0300

    коррекция main

--- lamborghini/js/main.js
+++ lamborghini/js/main.js
@@ -36,6 +36,31 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function piusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo-cursor");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i= 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
 
 
 
